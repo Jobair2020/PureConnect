ea8b8294fb82920d1409ae97e5561b2a89142978
kuet <kristysaha@gmail.com>
2023-09-05 12:41:45 +0600

    Initial Commit

---
+++ PureConnect/PureConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PureConnectApp.swift
+//  PureConnect
+//
+//  Created by kuetcse on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct PureConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
